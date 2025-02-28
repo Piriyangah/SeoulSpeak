5d6580e67b8cc9e5acb3f167b407d363828dc88e
Piriyangah <Piriyangah.Muthurasa@student.htw-berlin.de>
2025-02-28 19:27:25 +0100

    Frontend Ordnerstructur

---
+++ frontend/src/app/pages/vocab/vocab.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vocab',
+  imports: [],
+  templateUrl: './vocab.component.html',
+  styleUrl: './vocab.component.css'
+})
+export class VocabComponent {
+
+}
