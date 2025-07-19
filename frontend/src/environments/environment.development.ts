29febf6e4aae4e72ac4adb2102677af99041e934
Piriyangah <Piriyangah.Muthurasa@student.htw-berlin.de>
2025-07-19 02:58:22 +0200

    apiUrl verändert

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,13 @@
 // Entwicklungsumgebung beschreiben
-export const environment = {
+/*export const environment = {
     production: false,
     apiUrl: 'http://localhost:3000',
+};
+*/
+
+// Render verbinden
+export const environment = {
+  production: true,
+  apiUrl: 'https://seoulspeak.onrender.com'
+  // man soll mit this.http.get(`${environment.apiUrl}/vocabulary`) auhc machch können --- Später ausprobieren
 };
